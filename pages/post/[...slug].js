90abf37a73fa6513d46942c608868ed875da5221
suppanut.sittimakarn <arisomna1878@gmail.com>
2022-10-22 21:05:06 +0700

    try dynamic route

---
+++ pages/post/[...slug].js
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+
+function Sluggering() {
+  const router = useRouter();
+  const listOfParams = router.query.slug;
+
+  return (
+    <div>
+      this is not index
+      {listOfParams?.map((value, key) => {
+        return (
+          <div key={key}>
+            <p>{value}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Sluggering;
--- pages/profile/[username].js
+++ pages/profile/[username].js
@@ -2,8 +2,15 @@ import { useRouter } from "next/router.js";
 
 const Profile = () => {
   const router = useRouter();
-  const { username } = router.query;
-  return <div>Hello {username}!</div>;
+  const { username, a, b } = router.query;
+  return (
+    <div>
+      <p>Hello {username}!</p>
+      <p>
+        and also {a} =---= {b}
+      </p>
+    </div>
+  );
 };
 
 export default Profile;
