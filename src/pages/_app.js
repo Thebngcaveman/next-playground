424dc2e5547411b3f8bc05db55241ef467cb16bf
suppanut.sittimakarn <arisomna1878@gmail.com>
2022-10-23 11:37:22 +0700

    add material ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,30 @@
 import Layout from "../components/Layout.js";
 import "../styles/globals.css";
 import Head from "next/head.js";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../mui-config/theme";
+import createEmotionCache from "../mui-config/createEmotionalCache.jsx";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 function App({ Component, pageProps }) {
+  const { emotionCache = clientSideEmotionCache } = pageProps;
   return (
-    <Layout>
+    <CacheProvider value={emotionCache}>
       <Head>
-        <title>Damedane tech Tutorial</title>
+        <title>Ricardo Milos next moves</title>
       </Head>
-      <Component {...pageProps} />
-    </Layout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
